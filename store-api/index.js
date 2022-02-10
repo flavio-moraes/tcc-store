1ab1f1723e6a35f10fa2d8532562f4e36452c4c4
Flávio Moraes <flavio-bp@hotmail.com>
2022-02-10 19:20:47 -0300

    Rotas e CRUDs

--- store-api/index.js
+++ store-api/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 dotenv.config();
 
@@ -28,6 +31,9 @@ app.get("/api/v1/test", () => {
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productRoute);
+app.use("/api/v1/carts", cartRoute);
+app.use("/api/v1/orders", orderRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
--- store-api/routes/cart.js
+++ store-api/routes/cart.js
@@ -1,5 +1,71 @@
 const router = require("express").Router();
+const Cart = require("../models/Cart");
+const {verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin} = require("./verifyToken");
 
 
+//Criar um carrinho de compra
+router.post("/", verifyToken, async (req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}) 
+
+
+//Atualizar
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body},
+            {new: true}
+        );
+        res.status(200).json(updatedCart);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//Deletar
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Carrinho deletado.");
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//Ler um
+router.get("/:userId", verifyTokenAndAuth, async (req, res) => {
+    try {
+        const cart = await Cart.findOne({userId: req.params.userId});
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//Ler todos
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+
+    try {
+        const carts = await Cart.find();
+        res.status(200).json(carts);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
